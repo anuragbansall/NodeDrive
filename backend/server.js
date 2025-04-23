de6db3c4b650ed1adfe61ba6fb6d71e7e5c6768f
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-23 17:00:13 +0530

    Add Error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const errStatus = err.status || 500;
+  const errMsg = err.message || "Something went wrong!";
+
+  console.error(err.stack);
+  return res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMsg,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
