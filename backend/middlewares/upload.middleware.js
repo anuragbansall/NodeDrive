8b83103c4cfd25c74cdfb9c484bcb97faaa48eb3
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-24 17:39:57 +0530

    Change cloudinary folder name

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -5,7 +5,7 @@ import cloudinary from "../config/cloudinary.js";
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "uploads",
+    folder: "NodeDrive",
     resource_type: "auto", // Will auto-select "raw" for PDFs, videos, etc.
     format: async (req, file) => file.originalname.split(".").pop(), // Keep original extension
     public_id: (req, file) => `${Date.now()}-${file.originalname}`,
