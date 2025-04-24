7e23d348fbb506de146b513e21ffbadb32d30328
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-24 16:35:22 +0530

    Add File Model

---
+++ backend/models/File.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    publicUrl: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const File = mongoose.models.File || mongoose.model("File", fileSchema);
+export default File;
