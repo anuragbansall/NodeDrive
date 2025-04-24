8b090b3604e7b2b0f0f6fb045929ad67f86b72b3
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-24 15:20:19 +0530

    Authorize Upload Route

--- backend/controllers/Upload.controller.js
+++ backend/controllers/Upload.controller.js
@@ -1,6 +1,7 @@
 export const uploadFile = async (req, res, next) => {
   try {
     const file = req.file;
+    const user = req.user;
 
     if (!file) {
       const error = new Error("Please upload a file");
@@ -17,6 +18,10 @@ export const uploadFile = async (req, res, next) => {
         size: file.size,
         type: file.mimetype,
         buffer: file.buffer.toString("base64"),
+      },
+      user: {
+        id: user.id,
+        name: user.name,
       },
     });
   } catch (error) {
--- backend/routes/Upload.routes.js
+++ backend/routes/Upload.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../middlewares/upload.middleware.js";
 import { uploadFile } from "../controllers/Upload.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const uploadRouter = express.Router();
 
-uploadRouter.post("/", upload.single("file"), uploadFile); // Upload a single file with the field name "file"
+uploadRouter.post("/", authorize, upload.single("file"), uploadFile); // Upload a single file with the field name "file"
 
 export default uploadRouter;
