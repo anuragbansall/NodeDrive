bf88701fbc8cdd83624f487e1ab59a0efc442c8c
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-24 17:48:19 +0530

    Change Date to UUID in  public_id

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
+import { v4 as uuidv4 } from "uuid";
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
@@ -8,7 +9,7 @@ const storage = new CloudinaryStorage({
     folder: "NodeDrive",
     resource_type: "auto", // Will auto-select "raw" for PDFs, videos, etc.
     format: async (req, file) => file.originalname.split(".").pop(), // Keep original extension
-    public_id: (req, file) => `${Date.now()}-${file.originalname}`,
+    public_id: (req, file) => `${uuidv4()}-${file.originalname}`,
     access_mode: "public",
   },
 });
