363ab80d431d794e729c25aa385df1fb4f19cece
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-24 09:39:57 +0530

    Update User Model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      minlength: [6, "Password must be at least 6 characters long"],
     },
     role: {
       type: String,
