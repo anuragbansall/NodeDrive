c7fe3c17192b8a59b53c28d3814b8dad2e805ee3
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-28 23:54:00 +0530

    make user routes clean

--- backend/routes/User.routes.js
+++ backend/routes/User.routes.js
@@ -1,27 +1,30 @@
 import express from "express";
 import { login, logout, register } from "../controllers/User.controller.js";
 import { body } from "express-validator";
-import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post(
   "/register",
-  body("name").notEmpty().withMessage("Name is required"),
-  body("email").isEmail().withMessage("Email is required"),
-  body("password")
-    .trim()
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters long"),
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Email is required"),
+    body("password")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
   register
 );
 
 userRouter.post(
   "/login",
-  body("email").isEmail().withMessage("Email is required"),
-  body("password")
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters long"),
+  [
+    body("email").isEmail().withMessage("Email is required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
   login
 );
 userRouter.post("/logout", logout);
